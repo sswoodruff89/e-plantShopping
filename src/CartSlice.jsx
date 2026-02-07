8f8e99a9d9b0e51b42de27571660c516a428cce7
Sean Woodruff <sswoodruff89@gmail.com>
2026-02-07 15:52:36 -0500

    updated cart slice

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,23 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
+        const { name, image, cost } = action.payload;
+        const cartItem = state.items.find((item) => item.name === name);
+        if (cartItem) {
+            cartItem.quantity++;
+        } else {
+            state.items.push({ name, image, cost, quantity: 1 });
+        }
     
     },
     removeItem: (state, action) => {
+        return state.items.filter((item) => item.name === action.payload.name);
     },
     updateQuantity: (state, action) => {
-
-    
+        const { name, quantity } = action.payload;
+        const itemIndex = state.items.findIndex((item) => item.name === name);
+        state.items[itemIndex]?.quantity = quantity;
+        return state;
     },
   },
 });
